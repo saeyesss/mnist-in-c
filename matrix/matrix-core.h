fd57f430afcafd21fb01e5084f53ca6e115a6e63
saeyesss <94211639+saeyesss@users.noreply.github.com>
2022-06-04 21:36:02 +0530

    init

---
+++ matrix/matrix-core.h
@@ -0,0 +1,17 @@
+#pragma once
+
+typedef struct{
+
+    double **entries;
+    int rows;
+    int cols;
+} Matrix;
+
+Matrix *matrix_create(int row, int col);
+void matrix_fill(Matrix *m, int n);
+void matrix_free(Matrix *m);
+void matrix_print(Matrix *m);
+
+Matrix *matrix_copy(Matrix *m);
+void matrix_save(Matrix *m, char *file_string);
+Matrix *matrix_load(char *file_string);
